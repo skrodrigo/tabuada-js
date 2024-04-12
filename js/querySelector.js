fce56070f443f3232a6452340eee0e6a3e71df25
Rodrigo Carvalho <142619236+skrodrigo@users.noreply.github.com>
2024-04-11 21:27:17 -0300

    Add files via upload
    feat: create index, manifest

---
+++ js/querySelector.js
@@ -0,0 +1,4 @@
+export var numbers = document.querySelectorAll(".rc-numbers div");
+export var operations = document.querySelectorAll(".rc-operations div");
+export var display = document.querySelector(".rc-display");
+
---
+++ servicesWorker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'tabuada_js';
+const assetsToCache = [
+    '/index.html',
+    '/css/style.css',
+    '/js/index.js',
+    '/js/querySelector.js',
+    '/manifest.json'
+];
+
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(assetsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+        return Promise.all(keys.map(key => {
+            if (key !== CACHE_NAME) {
+            return caches.delete(key);
+            }
+        }));
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+        return cachedResponse || fetch(event.request);
+        })
+    );
+});
